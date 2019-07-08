64ddfda72436789c5850807d95d6ca3d9f0f653f
deboshas <debocognizant@gmail.com>
2019-07-08 09:25:58 +0530

    Specc fiel for e2e

---
+++ src/app/voter/voter.component.spec.ts
@@ -0,0 +1,54 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { VoterComponent } from './voter.component';
+
+describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
+
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [VoterComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should render total votes as 21', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+    let de = fixture.debugElement.query(By.css(".vote-count"));
+    console.log(de);
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain("21");
+  });
+  it('should highlight upvote button if click on upvote', () => {
+    component.othersVote = 20;
+    component.myVote = 9;
+    fixture.detectChanges();
+    let de = fixture.debugElement.query(By.css(".glyphicon-menu-up"));
+    console.log(de);
+    expect(de.classes['highlighted']).toBeTruthy();
+
+  });
+  it('should increse total votes when click on upvote button', () => {
+
+
+    let button = fixture.debugElement.query(By.css(".glyphicon-menu-up"));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(component.totalVotes).toBe(1);
+
+  });
+});
